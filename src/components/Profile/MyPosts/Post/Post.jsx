724d61ce676b9aecdf1f64b93a36e17679b67068
DmitriyTavluy <tavluy1999@mail.ru>
2022-02-19 15:22:58 +0300

    first commit

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Posts.module.css';
+
+const Posts = () => {
+    return (
+
+                <div className={s.item}>
+                    <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" alt=""/>
+                    post 1
+                </div>
+
+    );
+};
+
+export default Posts;
